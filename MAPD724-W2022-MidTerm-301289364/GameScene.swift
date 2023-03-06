808ffdb885d13993b786e3d0e6009b3361ba1840
Hisham Sanimeh <h.snaimeh93@gmail.com>
2023-03-05 22:26:42 -0500

    Add managers and finish Controller

--- MAPD724-W2022-MidTerm-301289364/GameScene.swift
+++ MAPD724-W2022-MidTerm-301289364/GameScene.swift
@@ -1,76 +1,108 @@
-//
-//  GameScene.swift
-//  MAPD724-W2022-MidTerm-301289364
-//
-//  Created by Hisham Sanimeh on 05/03/2023.
-//
-
 import SpriteKit
 import GameplayKit
+import AVFoundation
+import UIKit
+
+let screenSize = UIScreen.main.bounds
+var screenWidth: CGFloat?
+var screenHeight: CGFloat?
 
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var ocean: Ocean?
+    var secondOcean: Ocean?
+    var player: Player?
+    var island: Island?
+    var clouds : [Cloud] = []
     
     override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        initGameObject()
+        initGameSound()
+        
+    }
+    
+    func initGameObject(){
+        name = "GAME"
+        
+        // add the first ocean to the Scene
+        ocean = Ocean()
+        ocean?.Reset()
+        addChild(ocean!)
+        
+        // add the second ocean to the Scene
+        secondOcean = Ocean()
+        //rotate the assets by 90 degrees
+        secondOcean?.zRotation =  .pi / 2
+        secondOcean?.position.x = -627.5
+        addChild(secondOcean!)
+        
+        
+        //add the player to the scene
+        player = Player()
+        player?.Reset()
+        addChild(player!)
+        
+        //add the island to the scene
+        island = Island()
+        addChild(island!)
+        
+        
+        // add 2 clouds to the Scene
+        for _ in 0...1
+        {
+            let cloud = Cloud()
+            clouds.append(cloud)
+            addChild(cloud)
         }
+    }
+    
+    
+    func initGameSound(){
+        // Init Engine Sound and  Background noise / music
+        let engineSound = SKAudioNode(fileNamed: "engine.mp3")
+        addChild(engineSound)
+        engineSound.autoplayLooped = true
+        engineSound.run(SKAction.changeVolume(to: 0.5, duration: 0))
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        // preload / prewarm impulse sounds
+        do
+        {
+            let sounds: [String] = ["thunder", "yay"]
+            for sound in sounds
+            {
+                let path: String = Bundle.main.path(forResource: sound, ofType: "mp3")!
+                let url:URL = URL(fileURLWithPath: path)
+                let avPlayer: AVAudioPlayer = try AVAudioPlayer(contentsOf: url)
+                
+                avPlayer.prepareToPlay()
+            }
+        }
+        catch
+        {
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
         }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    func touchDown(atPoint pos : CGPoint)
+    {
+        player?.TouchMove(newPos: CGPoint(x: pos.x, y: pos.y))
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+    func touchMoved(toPoint pos : CGPoint)
+    {
+        player?.TouchMove(newPos: CGPoint(x: pos.x, y: pos.y))
     }
     
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+    func touchUp(atPoint pos : CGPoint)
+    {
+        player?.TouchMove(newPos: CGPoint(x: pos.x, y: pos.y))
     }
+    
+    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
@@ -89,21 +121,20 @@ class GameScene: SKScene {
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        ocean?.Update()
+        secondOcean?.Update()
+        player?.Update()
+        island?.Update()
         
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
+        // update each cloud in the clouds array
+        for cloud in clouds
+        {
+            cloud.Update()
+        // Check if they have a collision between the player and the cloud
+            CollisionManager.SquaredRadiusCheck(scene: self, object1: player!, object2: cloud)
         }
         
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
+        // Check if they have a collision between the player and the island
+        CollisionManager.SquaredRadiusCheck(scene: self, object1: player!, object2: island!)
     }
 }
--- MAPD724-W2022-MidTerm-301289364/GameViewController.swift
+++ MAPD724-W2022-MidTerm-301289364/GameViewController.swift
@@ -1,55 +1,78 @@
-//
-//  GameViewController.swift
-//  MAPD724-W2022-MidTerm-301289364
-//
-//  Created by Hisham Sanimeh on 05/03/2023.
-//
 
 import UIKit
 import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var LivesLabel: UILabel!
+    @IBOutlet weak var ScoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
+        
         if let scene = GKScene(fileNamed: "GameScene") {
             
-            // Get the SKScene from the loaded GKScene
             if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
                 sceneNode.scaleMode = .aspectFill
                 
-                // Present the scene
+                
                 if let view = self.view as! SKView? {
                     view.presentScene(sceneNode)
                     
                     view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
                 }
             }
         }
+        // Initialize the Lives and Score
+        CollisionManager.gameViewController = self
+        ScoreManager.Score = 0
+        ScoreManager.Lives = 5
+        updateLivesLabel()
+        updateScoreLabel()
+        
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return  .landscape
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
+    func updateLivesLabel()
+    {
+        if ScoreManager.Lives == 0{
+            ScoreManager.Lives = 5
+            ScoreManager.Score = 0
+            self.showToast(message: "Game over...", seconds: 1.0)
+            LivesLabel.text = "Lives: \(ScoreManager.Lives)"
+            ScoreLabel.text = "Score: \(ScoreManager.Score)"
+        }else{
+            LivesLabel.text = "Lives: \(ScoreManager.Lives)"
+            
+        }
+        
+    }
+    
+    func updateScoreLabel()
+    {
+        ScoreLabel.text = "Score: \(ScoreManager.Score)"
+    }
+    
+    func showToast(message : String, seconds: Double){
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alert.view.backgroundColor = .black
+        alert.view.alpha = 0.5
+        alert.view.layer.cornerRadius = 15
+        self.present(alert, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + seconds) {
+            alert.dismiss(animated: true)
+        }
+    }
+    
 }
+
